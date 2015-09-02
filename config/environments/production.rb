92ad1bc88c3cb7dedf04e8651bc4ce3be350088f
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-09-02 11:50:37 +0200

    Prod config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,11 +30,28 @@ module RouterWrapper
   CACHE = CacheManager.new(ActiveSupport::Cache::RedisStore.new(host: ENV['REDIS_HOST'] || 'localhost', namespace: 'router', expires_in: 60*60*24*1, raise_errors: true))
 
   CROW = Wrappers::Crow.new(CACHE)
-  OSRM5 = Wrappers::Osrm5.new(CACHE, url_time: 'http://router.project-osrm.org', url_distance: 'http://router.project-osrm.org', url_isochrone: 'http://localhost:1723', url_isodistance: 'http://localhost:1723', licence: 'ODbL', attribution: '© OpenStreetMap contributors')
-  OSRM5_CAR_ICELAND = Wrappers::Osrm5.new(CACHE, url_time: 'http://osrm-car-iceland:5000', url_distance: nil, url_isochrone: 'http://osrm-car-iceland:6000', url_isodistance: nil, licence: 'ODbL', attribution: '© OpenStreetMap contributors')
-  OTP_BORDEAUX = Wrappers::Otp.new(CACHE, url: 'http://otp:8080', router_id: 'bordeaux', licence: 'ODbL', attribution: 'Bordeaux Métropole', area: 'Bordeaux', crs: 'EPSG:2154')
-  HERE_APP_ID = nil
-  HERE_APP_CODE = nil
+  OSRM_CAR_EUROPE = Wrappers::Osrm4.new(CACHE, url_time: 'http://localhost:5000', url_distance: 'http://localhost:5004', url_isochrone: 'http://localhost:6000', url_isodistance: 'http://localhost:6004', licence: 'ODbL', attribution: '© OpenStreetMap contributors', area: 'Europe')
+  OSRM_CAR_URBAN_FRANCE = Wrappers::Osrm4.new(CACHE, url_time: 'http://localhost:5003', url_distance: 'http://localhost:5004',url_isochrone: 'http://localhost:6003', url_isodistance: 'http://localhost:6004', licence: 'ODbL', attribution: '© OpenStreetMap contributors', area: 'France')
+  OSRM_PEDESTRIAN_FRANCE = Wrappers::Osrm4.new(CACHE, url_time: 'http://localhost:5002', url_isochrone: 'http://localhost:6002', licence: 'ODbL', attribution: '© OpenStreetMap contributors', area: 'France')
+  OSRM_CYCLE_FRANCE = Wrappers::Osrm4.new(CACHE, url_time: 'http://localhost:5001', url_isochrone: 'http://localhost:6001', licence: 'ODbL', attribution: '© OpenStreetMap contributors', area: 'France')
+
+  OTP = {
+    bordeaux: {licence: 'ODbL', attribution: 'Bordeaux Métropole', area: 'Bordeaux, France', boundary: 'poly/france-bordeaux.kml', crs: 'EPSG:2154'},
+    nantes: {licence: 'ODbL', attribution: 'Nantes Métropole', area: 'Nantes, France', boundary: 'poly/france-nantes.kml', crs: 'EPSG:2154'},
+    toulouse: {licence: 'ODbL', attribution: 'Tisséo', area: 'Toulouse, France', boundary: 'poly/france-toulouse.kml', crs: 'EPSG:2154'},
+    metz: {licence: 'LO', attribution: 'Metz Métropole', area: 'Metz, France', boundary: 'poly/france-metz.kml', crs: 'EPSG:2154'},
+    nancy: {licence: 'ODbL', attribution: 'Communauté Urbaine du Grand Nancy', area: 'Nancy, France', boundary: 'poly/france-nancy.kml', crs: 'EPSG:2154'},
+    rennes: {licence: 'ODbL', attribution: 'STAR', area: 'Rennes, France', boundary: 'poly/france-rennes.kml', crs: 'EPSG:2154'},
+    strasbourg: {licence: '', attribution: 'Compagnie des Transports Strasbourgeois', area: 'Strasbourg, France', boundary: 'poly/france-strasbourg.kml', crs: 'EPSG:2154'},
+    idf: {licence: 'ODbL', attribution: 'STIF', area: 'Île-de-France, France', boundary: 'poly/france-idf.kml', crs: 'EPSG:2154'},
+    grenoble: {licence: 'ODbL', attribution: 'Grenoble Alpes Métropole', area: 'Grenoble, France', boundary: 'poly/france-grenoble.kml', crs: 'EPSG:2154'},
+    marseille: {licence: 'ODbL', attribution: 'Syndicat Mixte des Transports des Bouches-du-Rhône', area: 'Marseille, France', boundary: 'poly/france-marseille.kml', crs: 'EPSG:2154'},
+  }.collect{ |k, v|
+    Wrappers::Otp.new(CACHE, v.merge(url: 'http://localhost:7000', router_id: k.to_s))
+  }
+
+  HERE_APP_ID = 'yihiGwg1ibLi0q6BfBOa'
+  HERE_APP_CODE = '5GEGWZnjPAA-ZIwc7DF3Mw'
   HERE_TRUCK = Wrappers::Here.new(CACHE, app_id: HERE_APP_ID, app_code: HERE_APP_CODE, mode: 'truck')
 
   @@c = {
@@ -42,36 +59,70 @@ module RouterWrapper
     product_contact_email: 'tech@mapotempo.com',
     product_contact_url: 'https://github.com/Mapotempo/router-wrapper',
     profiles: [{
-      api_keys: ['light'],
+      api_keys: [
+        'demo',
+        'apologic-1-9f9f5d62b4c32ce08f7f1bd144133e088f59c445',
+        'apologic-beta-79728b4dbd59e080d36ba862d592d694',
+      ],
       services: {
-        route_default: :crow,
+        route_default: :car,
         route: {
+          car: [OSRM_CAR_EUROPE],
+          car_urban: [OSRM_CAR_URBAN_FRANCE],
+          pedestrian: [OSRM_PEDESTRIAN_FRANCE],
+          cycle: [OSRM_CYCLE_FRANCE],
+          public_transport: OTP,
           crow: [CROW],
         },
         matrix: {
+          car: [OSRM_CAR_EUROPE],
+          car_urban: [OSRM_CAR_URBAN_FRANCE],
+          pedestrian: [OSRM_PEDESTRIAN_FRANCE],
+          cycle: [OSRM_CYCLE_FRANCE],
+          public_transport: OTP,
           crow: [CROW],
         },
         isoline: {
+          car: [OSRM_CAR_EUROPE],
+          car_urban: [OSRM_CAR_URBAN_FRANCE],
+          pedestrian: [OSRM_PEDESTRIAN_FRANCE],
+          cycle: [OSRM_CYCLE_FRANCE],
+          public_transport: OTP,
           crow: [CROW],
         }
       }
     }, {
-      api_keys: ['demo'],
+      api_keys: [
+        'mapotempo-web-1-d701e4a905fbd3c8d0600a2af433db8b',
+      ],
       services: {
-        route_default: :osrm5,
+        route_default: :car,
         route: {
-          osrm5: [OSRM5_CAR_ICELAND, OSRM5],
-          otp: [OTP_BORDEAUX],
-          here: [HERE_TRUCK],
+          car: [OSRM_CAR_EUROPE],
+          car_urban: [OSRM_CAR_URBAN_FRANCE],
+          pedestrian: [OSRM_PEDESTRIAN_FRANCE],
+          cycle: [OSRM_CYCLE_FRANCE],
+          public_transport: OTP,
+          crow: [CROW],
+          truck: [HERE_TRUCK],
         },
         matrix: {
-          osrm5: [OSRM5_CAR_ICELAND, OSRM5],
-          otp: [OTP_BORDEAUX],
-          here: [HERE_TRUCK],
+          car: [OSRM_CAR_EUROPE],
+          car_urban: [OSRM_CAR_URBAN_FRANCE],
+          pedestrian: [OSRM_PEDESTRIAN_FRANCE],
+          cycle: [OSRM_CYCLE_FRANCE],
+          public_transport: OTP,
+          crow: [CROW],
+          truck: [HERE_TRUCK],
         },
         isoline: {
-          osrm5: [OSRM5_CAR_ICELAND, OSRM5],
-          otp: [OTP_BORDEAUX],
+          car: [OSRM_CAR_EUROPE],
+          car_urban: [OSRM_CAR_URBAN_FRANCE],
+          pedestrian: [OSRM_PEDESTRIAN_FRANCE],
+          cycle: [OSRM_CYCLE_FRANCE],
+          public_transport: OTP,
+          crow: [CROW],
+          truck: [HERE_TRUCK],
         }
       }
     }]
